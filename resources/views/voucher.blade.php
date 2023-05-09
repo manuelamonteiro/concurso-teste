fc79e2a3ae8d5f0456419b850c39e9b716f8bfd7
manuelamp <manuella.m.p182@gmail.com>
2023-05-09 16:47:34 -0300

    fix: voucher page layout

--- resources/views/voucher.blade.php
+++ resources/views/voucher.blade.php
@@ -12,37 +12,37 @@
         <div class="form-info">
             <div class="left-form">
                 <label for="id-voucher">Inscrição:</label>
-                <input type="text" id="id-voucher" readonly value="" />
+                <input type="text" id="id-voucher" readonly value="" style="background-color: #eeeeee"/>
 
                 <label for="name">Nome completo:</label>
-                <input type="text" id="name-voucher" readonly value="" />
+                <input type="text" id="name-voucher" readonly value="" style="background-color: #eeeeee"/>
 
                 <label for="address">Endereço:</label>
-                <input type="text" id="address-voucher" readonly value="" />
+                <input type="text" id="address-voucher" readonly value="" style="background-color: #eeeeee"/>
 
                 <label for="role">Cargo:</label>
-                <input type="text" id="role-voucher" readonly value="" />
+                <input type="text" id="role-voucher" readonly value="" style="background-color: #eeeeee"/>
             </div>
 
             <div class="right-form">
                 <div class="situation-date">
                     <label for="situation">Situação:</label>
-                    <input type="text" id="situation-voucher" readonly value="" />
+                    <input type="text" id="situation-voucher" readonly value="" style="background-color: #eeeeee"/>
 
                     <label for="date">Data de Inscrição:</label>
-                    <input type="text" id="date-voucher" readonly value="" />
+                    <input type="text" id="date-voucher" readonly value="" style="background-color: #eeeeee"/>
                 </div>
 
                 <label for="CPF">CPF:</label>
-                <input type="text" id="CPF-voucher" readonly value=""/>
+                <input type="text" id="CPF-voucher" readonly value="" style="background-color: #eeeeee"/>
 
                 <div class="city-state-form">
                     <label for="state">Estado:</label>
-                    <input type="text" id="state-voucher" readonly value="" />
+                    <input type="text" id="state-voucher" readonly value="" style="background-color: #eeeeee"/>
                     </select>
 
                     <label for="city">Cidade:</label>
-                    <input type="text" id="city-voucher" readonly value="" />
+                    <input type="text" id="city-voucher" readonly value="" style="background-color: #eeeeee"/>
                     </select>
                 </div>
             </div>
